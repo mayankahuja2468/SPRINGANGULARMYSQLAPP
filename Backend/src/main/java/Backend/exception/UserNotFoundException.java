eaa9614d8a82fc484c4847b9ec45a9974a6af52d
Mayank Ahuja <Mayank Ahuja>
2022-12-29 13:05:01 +0530

    Commit #2

---
+++ Backend/src/main/java/Backend/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package Backend.exception;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ Backend/src/main/java/Backend/repo/EmployeeRepo.java
@@ -0,0 +1,12 @@
+package Backend.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import Backend.model.Employee;
+
+import java.util.Optional;
+
+public interface EmployeeRepo extends JpaRepository<Employee, Long> {
+    void deleteEmployeeById(Long id);
+
+    Optional<Employee> findEmployeeById(Long id);
+}
